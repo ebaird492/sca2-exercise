ed3a55b3faa57885a282bf95a42318d9eea97860
ebaird492 <ebaird492@gmail.com>
2021-03-20 15:44:33 -0600

    Most drills

--- 1_AM/drills.js
+++ 1_AM/drills.js
@@ -3,12 +3,18 @@ const assert = require("assert")
 // Feel free to look things up online!
 
 const reverse = function(str) {
-  // TODO - write a function which reverses the string
+  revString = ""
+  for (let i = str.length - 1; i >= 0; i--) {
+    revString += str[i]
+  }
+  return revString
 }
 
 const factorial = function(num) {
-  // TODO - write a function which returns the factorial of a positive integer
-  // For example, 5! = 5 * 4 * 3 * 2 * 1 = 120
+  if (num == 0)
+    return 1
+  else
+    return num * factorial(num-1)
 }
 
 const announceDate = function() {
@@ -17,26 +23,49 @@ const announceDate = function() {
 }
 
 const shiftRight = function(str, num) {
-  // TODO - write a function which shifts a string `num` characters to the right
-  // Example = shiftThree("Hello") === "lloHe"
+  let tempStr = []
+  for (let i = 0; i < str.length; i++) {
+    let index = i + num
+    index = index % str.length
+    tempStr[index] = str[i]
+  }
+  return tempStr.toString().replace(/,/g, "")
 }
 
 const tokenize = function(str) {
-  // TODO - write a function which converts a multi-word string into an array of words
+  return str.split(" ")
 }
 
 const uniqueOnes = function(ary) {
-  // TODO - write a function which returns the inputted array without duplicate elements
+  let uniqueArr = []
+  for (let i = 0; i < ary.length; i++) {
+    if (uniqueArr.indexOf(ary[i]) == -1) {
+      uniqueArr.push(ary[i])
+    }
+  }
+  return uniqueArr
 }
 
 const zip = function(arr1, arr2) {
-  // TODO - write a function which combines two arrays into an array of 2-element arrays and returns -1
-  // if the two arrays are of unequal length
-  // Example: zip([1, 2, 3], [5, 3, 1]) === [[1, 5], [2, 3], [3, 1]]
+  if (arr1.length != arr2.length)
+    return -1
+  else {
+    let zipArr = []
+    for (let i = 0; i < arr1.length; i++) {
+      zipArr[i] = [arr1[i], arr2[i]]
+    }
+    return zipArr
+  }
 }
 
 const unzip = function(arr) {
-  // TODO - Write a function which does the opposite of `zip()`
+  arr1 = []
+  arr2 = []
+  for (let i = 0; i < arr.length; i++) {
+    arr1[i] = arr[i][0]
+    arr2[i] = arr[i][1]
+  }
+  return [arr1, arr2]
 }
 
 // Write tests here:
@@ -45,3 +74,12 @@ assert(1 < 2)
 assert(1 + 2 == 3)
 assert([2, 3][0] === 2)
 // assert(reverse("3df") === "fd3")
+console.log(reverse("burrito")) // returns "otirrub"
+console.log(factorial(5)) // returns 120
+console.log(announceDate()) // returns current date and time
+console.log(shiftRight("hello", 3)) // returns "llohe"
+console.log(tokenize("hello my name is ethan")) // returns ["hello", "my", "name" , "is", "ethan"]
+console.log(uniqueOnes(["apples", "oranges", "apples"])) // retruns ["apples", "oranges"]
+console.log(zip([1,2,3],[2,3,4])) // returns [[1,2], [2,2], [3,4]]
+console.log(zip([1,2],[3,4,5])) // returns -1
+console.log(unzip([[1,2], [2,3], [3,4]])) // returns [[1,2,3] [2,3,4]]
